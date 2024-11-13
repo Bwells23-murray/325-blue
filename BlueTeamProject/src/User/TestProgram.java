d7895217017c0aae17968ba041df43c61c01c5bd
Ethan-sexton <esexton2@murraystate.edu>
2024-11-13 16:36:56 -0600

    (hopefully) Fixed errors with the file paths by making it relative to the class files.

--- BlueTeamProject/src/User/TestProgram.java
+++ BlueTeamProject/src/User/TestProgram.java
@@ -7,14 +7,20 @@ import Skill.*;
 public class TestProgram {
 
     public static void main(String[] args) throws IOException {
-        Manager m = new Manager();
-        m.createEmployee("0142", "Dave", "Matthew");
-        m.createEmployee("3256", "Jim", "Jimminy");
-        m.createEmployee("9889", "Bobby", "ybboB");
-        m.createEmployee("7462", "Josephine", "Clark");
-        m.createEmployee("6327", "Cantthink", "Ofmoore");
-        m.deleteEmployee("3256");
-        m.editEmployee("0142", "Matthews", 3);
+        try {
+            Manager m = new Manager();
+            m.createEmployee("0142", "Dave", "Matthew");
+            m.createEmployee("3256", "Jim", "Jimminy");
+            m.createEmployee("9889", "Bobby", "ybboB");
+            m.createEmployee("7462", "Josephine", "Clark");
+            m.createEmployee("6327", "Cantthink", "Ofmoore");
+            m.deleteEmployee("3256");
+            m.editEmployee("0142", "Matthews", 3);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
 
 
       
